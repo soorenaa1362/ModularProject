1490339871cda16cfdbc0eb04a0a253edfacb84b
soorenaa1362 <soorenaa1362@gmail.com>
2021-09-23 20:55:16 +0330

    08 ServiceProvider for User Module

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        \Soorenaa\User\Providers\UserServiceProvider::class,
 
         /*
          * Package Service Providers...
