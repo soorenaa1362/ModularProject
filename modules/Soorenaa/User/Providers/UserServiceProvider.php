85700b28956da48693706bcb609be9d0bedede92
soorenaa1362 <soorenaa1362@gmail.com>
2021-09-24 11:10:53 +0330

    12 Transfer Migration and Factories of User Module

--- modules/Soorenaa/User/Providers/UserServiceProvider.php
+++ modules/Soorenaa/User/Providers/UserServiceProvider.php
@@ -6,23 +6,10 @@ use Illuminate\Support\ServiceProvider;
 
 class UserServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/../Routes/user_routes.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        $this->loadFactoriesFrom(__DIR__ . '/../Database/Factories');
     }
 }
