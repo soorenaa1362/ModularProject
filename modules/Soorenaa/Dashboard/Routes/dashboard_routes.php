1e0163c2e79c987c52694dd98106d1e7f7cff877
soorenaa1362 <soorenaa1362@gmail.com>
2021-09-26 14:34:47 +0330

    17 Config Dashboard Middlewares

--- modules/Soorenaa/Dashboard/Routes/dashboard_routes.php
+++ modules/Soorenaa/Dashboard/Routes/dashboard_routes.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::group(['namespace' => 'Soorenaa\Dashboard\Http\Controllers'], function ($router) {
+Route::group(['namespace' => 'Soorenaa\Dashboard\Http\Controllers', 'middleware' => ['web', 'auth', 'verified']], function ($router) {
     $router->get('/home' , 'DashboardController@home')->name('home');
 });
