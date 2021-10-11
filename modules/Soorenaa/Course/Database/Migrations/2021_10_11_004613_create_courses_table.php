6f209390c2ecb2b577c5c03c247a3f157199b3d6
soorenaa1362 <soorenaa1362@gmail.com>
2021-10-11 05:10:07 +0330

    33 Courses Migration

--- modules/Soorenaa/Course/Database/Migrations/2021_10_11_004613_create_courses_table.php
+++ modules/Soorenaa/Course/Database/Migrations/2021_10_11_004613_create_courses_table.php
@@ -15,7 +15,20 @@ class CreateCoursesTable extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('teacher_id')->unsigned();
+            $table->bigInteger('category_id')->unsigned()->nullable();
+            $table->string('title');
+            $table->string('slug');
+            $table->float('priority')->nullable();
+            $table->string('price', 10);
+            $table->string('percent', 5);
+            $table->enum('type' , ['free' , 'cash']);
+            $table->enum('status' , ['completed' , 'not-completed' , 'lock']);
+            $table->text('body')->nullable();
             $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('SET NULL');
+            $table->foreign('teacher_id')->references('id')->on('users')->onDelete('CASCADE');
         });
     }
 
